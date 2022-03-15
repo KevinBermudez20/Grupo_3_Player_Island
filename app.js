926ef36c325eca56d5d4073d8cc0ae5cc0dc0554
Miguel Velasco <68866356+miguelvelascole@users.noreply.github.com>
2022-03-15 01:51:51 -0600

    Login screen

--- app.js
+++ app.js
@@ -7,18 +7,21 @@ const puerto = 3030;
 app.use(express.static(path.join(__dirname,'public')));
 app.listen(puerto, ()=>{
     console.log(`este servidor esta sirviendo en el puerto ${puerto}`);
-})
+});
+
 app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname,'views/home.html'))
-})
+});
 app.post('/', (req,res)=>{
-   /*  console.log(req._read()) */
     res.sendFile(path.join(__dirname,'views/home.html'))
-})
+});
 app.get('/detalle-producto', (req,res)=>{
-    res.sendFile(path.join(__dirname,'views/detalle-producto.html'))
-})
+    res.sendFile(path.join(__dirname,'views/detalle-producto.html'));
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(path.join(__dirname,'views/login.html'));
+});
 
 app.get('/ListaProductos', (req,res)=>{
-    res.sendFile(path.join(__dirname,'views/listaProductos.html'))
-})
+    res.sendFile(path.join(__dirname,'views/listaProductos.html'));
+});
